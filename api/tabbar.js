ef82e8bf8649046937564b7952a3df766d045410
lfj <liao_fengji@163.com>
2020-03-26 17:43:09 +0800

    初始化项目

---
+++ api/tabbar.js
@@ -0,0 +1,38 @@
+const tabList = [{
+    id: 1,
+    value: '推荐'
+  }, {
+    id: 2,
+    value: 'VIP'
+  }, {
+    id: 3,
+    value: '头条'
+  }, {
+    id: 4,
+    value: '小说'
+  }, {
+    id: 5,
+    value: '直播'
+  }, {
+    id: 6,
+    value: '儿童'
+  }, {
+    id: 7,
+    value: '在家上课'
+  }, {
+    id: 8,
+    value: '粤语'
+  }, {
+    id: 9,
+    value: '相声'
+  }, {
+    id: 10,
+    value: '人文'
+  },
+  {
+    id: 11,
+    value: '历史'
+  }
+]
+
+export default tabList;
---
+++ component/tabBarss/tabBar.js
@@ -0,0 +1,118 @@
+// pages/index/tabBar/tabBar.js
+
+import tabList from '../../api/tabbar.js'
+
+// const tabLists = tabList;
+// console.log('tabLists====>', tabLists)
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabList: tabList,
+    navScrollLeft: 0,
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    currentTab: 0,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+    let query = wx.createSelectorQuery();
+    query.select('.nav-item').boundingClientRect()
+    query.exec(res => {
+      console.log('res3', res)
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+  switchNav(event) {
+    var windowWidth = wx.getSystemInfoSync().windowWidth
+    console.log("windowWidth==>", windowWidth)
+    let query = wx.createSelectorQuery();
+    console.log("query==>", query)
+    query.selectAll('#user-1').boundingClientRect(function(rect) {
+      console.log('==============>', rect)
+    }).exec();
+
+    console.log('event====>', event)
+    var cur = event.currentTarget.dataset.current;
+    console.log('cur===>', cur)
+    //每个tab选项宽度占1/5
+    var singleNavWidth = (windowWidth / 5);
+    //tab选项居中                            
+    this.setData({
+      // navScrollLeft: (cur - 2) * singleNavWidth
+      navScrollLeft: singleNavWidth
+    })
+    if (this.data.currentTab == cur) {
+      return false;
+    } else {
+      this.setData({
+        currentTab: cur
+      })
+    }
+  },
+
+  switchTab(event) {
+    var cur = event.detail.current;
+    var singleNavWidth = this.data.windowWidth / 5;
+    this.setData({
+      currentTab: cur,
+      navScrollLeft: (cur - 2) * singleNavWidth
+    });
+  }
+})
